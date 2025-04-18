e797c17a922217e4b1c26751ecfbfe6cb9b74818
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-04-18 23:18:10 +0600

    Task 3

---
+++ Task 3/Task3_1.py
@@ -0,0 +1,13 @@
+# Convert the for loop in Chapter 1 Topic 10 to while loop
+
+# Execution Point
+
+num = 1
+
+while (num < 10):
+    print(num, end = " ")
+    
+    if num % 3 == 0:
+        print("")
+    num += 1
+    
