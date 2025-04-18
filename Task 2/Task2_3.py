3ee0963ca7abe1d9bcf290d1e20868291d234a17
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-04-18 23:08:09 +0600

    Task 2

--- Task 2/Task2_3.py
+++ Task 2/Task2_3.py
@@ -1,10 +1,22 @@
 a = 7
 b = 4
-# Perform NOT operation on a = 7 and b = 4
+# Perform a^b and explain it
 
 # Execution Point
 
 c = a^b
-d= b^a
+print(c)
 
-print(c, d)
+# ^ is XOR operation
+# a = 7 = 1 1 1
+# b = 4 = 1 0 0 
+# --------------
+# a ^ b = 0 1 1 = 3
+
+# Explanation:
+# 0 XOR 0 = 0
+# 0 XOR 1 = 1
+# 1 XOR 0 = 1
+# 1 XOR 1 = 0
+# LSB and 2nd bit has 1 XOR 0 = 1
+# MSB has 1 XOR 1 = 1 
